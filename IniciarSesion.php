11b6e1367304986f3112fc1712859b9d9f1bc23f
JahLopezM <jasonlmsktftw12@outlook.com>
2024-03-07 00:50:27 -0600

    Update IniciarSesion.php

--- IniciarSesion.php
+++ IniciarSesion.php
@@ -1,42 +1,44 @@
 <?php
 session_start();
 
+// Verificar si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $servername = "localhost";
     $username = "root";
-    $password = "Conexion";   
+    $password = "Conexion";
     $dbname = "tutienda";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("La conexión falló: " . $conn->connect_error);
-}
-
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("La conexión falló: " . $conn->connect_error);
     }
- 
+
     $correo = $_POST["correo"];
     $contrasena = $_POST["contrasena"];
 
-    $stmt = $conn->prepare("SELECT ID_usuario, Nombre, Contraseña FROM Usuarios WHERE CorreoElectronico = ?");
+    $stmt = $conn->prepare("SELECT ID_usuario, Nombre, Contraseña, Rol FROM Usuarios WHERE CorreoElectronico = ?");
     $stmt->bind_param("s", $correo);
     $stmt->execute();
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id_usuario, $nombre, $hashed_password);
+        $stmt->bind_result($id_usuario, $nombre, $hashed_password, $rol);
         $stmt->fetch();
 
         if (password_verify($contrasena, $hashed_password)) {
             $_SESSION["ID_usuario"] = $id_usuario;
             $_SESSION["Nombre"] = $nombre;
-            header("Location: Menu_Principal.php");  
-            exit();
+            $_SESSION["Rol"] = $rol; // Agregar el rol a la sesión
+
+            // Verificar el rol y redirigir
+            if ($rol == 'admin') {
+                header("Location: CentroControl.php");
+                exit();
+            } else {
+                header("Location: Menu_Principal.php");
+                exit();
+            }
         } else {
             $error = "Contraseña incorrecta.";
         }
@@ -47,6 +49,7 @@ if ($conn->connect_error) {
     $stmt->close();
     $conn->close();
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -56,13 +59,13 @@ if ($conn->connect_error) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión - Tucalzado.com</title>
-   
 </head>
 
 <body>
     <h2>Iniciar Sesión</h2>
-    
+
     <?php
+    // Mostrar mensaje de error solo si se ha enviado el formulario
     if (isset($error)) {
         echo "<p style='color: red;'>$error</p>";
     }
@@ -77,7 +80,6 @@ if ($conn->connect_error) {
 
         <button type="submit">Iniciar Sesión</button>
 
-         
         <div>
             <a href="OlvidarContrasena.php">¿No recuerdas tu contraseña?</a>
             <span> | </span>
